4349c27d05f3d06bb367af228e223b7172eb5f7e
Dasha Timeeva <d.timeeva@yandex.ru>
2026-07-01 21:52:57 +0500

    filtering

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,37 +1,47 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-//const compare = createComparison(defaultRules);
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
+    //console.log('elements:', elements)
+    //console.log('indexes:', indexes)
     // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach((elementName) => {
-        elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
-            const option = document.createElement("option");
-            option.value = name;
-            option.textContent = name;
-            return option;
-        }));
-    })
+    Object.keys(indexes)
+  .forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName])
+        .map(name => {
+          const option = document.createElement('option');
+
+          option.value = name;
+          option.textContent = name;
+
+          return option;
+        })
+    );
+  });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-        //if (action && action.name === 'clear') {
-          //  state[action.value] = '';
-            //elements[action.value].value = '';
-        //}
-
         if (action && action.name === 'clear') {
-            const parent = action.parentElement;
-            const input = parent.querySelector('input');
-            if (input) {
-                input.value = '';
-                const fieldName = action.getAttribute('data-field');
-                state[fieldName] = '';
-            }
+            let parentElement = action.parentElement
+            let inputElement = parentElement.querySelector('.input')
+            inputElement.value = ''
+            inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+            state[action.dataset.field] = ''
         }
 
-        const compare = createComparison(defaultRules);
+        const totalFrom = parseFloat(state.totalFrom) || undefined;
+        const totalTo = parseFloat(state.totalTo) || undefined;
+
+        if (totalFrom !== undefined || totalTo !== undefined) {
+            state.total = [totalFrom, totalTo];
+        }
+
+        delete state.totalFrom;
+        delete state.totalTo;
+
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
         return data.filter(row => compare(row, state));
